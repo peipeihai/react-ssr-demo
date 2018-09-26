6dc0b5a498b713d954dfd186bda9213cd97bf1ea
barretyi <barretyi@tencent.com>
2018-09-26 09:53:08 +0800

    使用`webpack-node-externals`模块忽略服务端对node_modules进行打包

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
     target: 'node',
@@ -11,6 +12,9 @@ const config = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, './build'),
     },
+
+    // 告诉 webpack 执行时不要打包任何 node_modules 中已有的依赖包
+    externals: [webpackNodeExternals()],
 };
 
 module.exports = merge(baseConfig, config);
